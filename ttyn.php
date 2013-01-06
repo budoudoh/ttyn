e2385fecd7ba9f99f599342c36380d7a5b67acf2
Basil Udoudoh <budoudoh@gmail.com>
2013-01-05 22:54:24 -0500

    changed ttyn

--- ttyn.php
+++ ttyn.php
@@ -2,6 +2,52 @@
     /*
 	 * TTYN.php: Main PHP file for TTYN Application
 	 */
+	 $appname = "Talk To Me Now";
+	 function initialChoice($event)
+	 {
+	 	if(false)
+		{
+			callRecorder();		
+		}
+		else 
+		{
+			sendMessage();
+		}
+	 }
+			 
+	 function callRecorder()
+	 {
+	 	
+	 }
+	 
+	 function sendMessage()
+	 {
+	 	ask("Please Leave your message", 
+		array(
+		        "timeout" => 15.0,
+		        "mode" => "speech",
+		        "speechCompleteTimeout" => 15.0,
+		        "onChoice" => "messageSender"
+		 ));
+		
+	 }
+	 
+	 function messageSender($event)
+	 {
+	 	if($numFrom)
+		{
+			say("Your Message has been sent. Thank you for using $appname");
+			hangup();
+			call($numFrom, array("network" => "SMS"));
+			say ($event->value);
+			hangup();
+		}
+		else 
+		{
+			say("Your Message could not be sent. Thank you for using $appname.");
+			hangup();	
+		}
+	 }
 	 
 	 if($currentCall) //Check if current call object exists
 	 {
@@ -9,10 +55,27 @@
 	 }
 	 else 
 	 {
-		if($numToCall)
+		if($numTo)
 		{
-			call($numToCall);	
-			say("Jeremy, if you are hearing this, then this app is working");
+			call($numTo);
+			
+			$initial_speach = "You are receiving a call from $appname.";
+			if($caller)
+			{
+				$initial_speach = $caller." is would like to chat with you using the $appname.";
+			}
+			
+			$initial_speach = $initial_speach." If you would like to continue, press 1. If not, press 2 to send the caller a message.";
+				
+			say($initial_speach);
+			
+			ask($initial_speach, array(
+			        "timeout" => 15.0,
+			        "mode" => "dtmf",
+			        "interdigitTimeout" => 2,
+			        "onChoice" => "initialChoice"
+			 ) ); 
+			 
 		}	 
 	 }
 ?>
