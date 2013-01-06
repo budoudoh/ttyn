8befdbb76b8dd7494cf6cba5dbf1eac398da7d53
Basil Udoudoh <budoudoh@gmail.com>
2013-01-06 06:23:16 -0500

    changed ttyn

--- ttyn.php
+++ ttyn.php
@@ -21,7 +21,7 @@
 	 	say("Your call has begun.");
 	 	while(true)
 		{
-			record("", array (
+			/*record("", array (
 			    "maxTime" => 60,
 			    "silenceTimeout" => 2,
 			    "transcriptionOutURI" => "https://doublewindsortech.com:8000/",
@@ -30,14 +30,14 @@
 			    "onSignal" => function($event){
 	 				say($event->value);		
 			    })
-			);
+			);*/
 			 
-			 /*startCallRecording("https://doublewindsortech.com:8000/Something", array (
+			 startCallRecording("https://doublewindsortech.com:8000/Something", array (
 			    "transcriptionOutURI" => "https://doublewindsortech.com:8000/",
 			    "transcriptionID" => $currentCall->sessionId)
-			);*/
+			);
 			
-			wait(5000, array (
+			wait(10000, array (
 			    "onSignal" => function($event){
 			    	_log("Testing");
 					stopCallRecording();
@@ -45,7 +45,7 @@
 			  	})
 			);  
 			
-			//stopCallRecording();
+			stopCallRecording();
 		}
 	 }
 	 
