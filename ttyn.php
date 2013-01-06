6045d4a882352680f5bf6b6befb63a0b0833075f
Basil Udoudoh <budoudoh@gmail.com>
2013-01-05 22:02:47 -0500

    changed ttyn

--- ttyn.php
+++ ttyn.php
@@ -5,24 +5,14 @@
 	 
 	 if($currentCall) //Check if current call object exists
 	 {
-		$parameters = $currentCall->getParameters();
-		
-		if(array_key_exists('numToCall', $parameters)) //This is a dial out situation
-		{
-			$number = $parameters['numToCall'];
-			call($number);	
-		}
-		else 
-		{
-			//send session ID to server
-			$session_id = $currentCall->id;
-		}
-		
-		say("Jeremy, if you are hearing this, then this app is working");
 		
 	 }
 	 else 
 	 {
-		say("Unfortunatly, your call could not be completed.");	 
+		if($numToCall)
+		{
+			call($numToCall);	
+			say("Jeremy, if you are hearing this, then this app is working");
+		}	 
 	 }
 ?>
