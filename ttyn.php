7324a3f7165d068e0ac4521e0ab7cc528e9b465e
Basil Udoudoh <budoudoh@gmail.com>
2013-01-05 20:58:23 -0500

    changed proof

---
+++ ttyn.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+	 * TTYN.php: Main PHP file for TTYN Application
+	 */
+	 
+	 if($currentCall)
+	 {
+	 	say("The currentCall Object does exist.");
+	 }
+	 else 
+	 {
+		say("The currentCall Object does not exist.");	 
+	 }
+?>
