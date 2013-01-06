2b8b48578cb1e59ca6970517b3af7c22bcd539e9
Basil Udoudoh <budoudoh@gmail.com>
2013-01-05 21:40:57 -0500

    changed ttyn

--- ttyn.php
+++ ttyn.php
@@ -3,13 +3,26 @@
 	 * TTYN.php: Main PHP file for TTYN Application
 	 */
 	 
-	 if($currentCall)
+	 if($currentCall) //Check if current call object exists
 	 {
-	 	$session_id = $currentCall->id;
-	 	say("The session ID is ".$session_id);
+		$parameters = $currentCall->getParameters();
+		
+		if(array_key_exists('numToCall', $parameters)) //This is a dial out situation
+		{
+			$number = $parameters['numToCall'];
+			call($number);	
+		}
+		else 
+		{
+			//send session ID to server
+			$session_id = $currentCall->id;
+		}
+		
+		say("Jeremy, if you are hearing this, then this app is working");
+		
 	 }
 	 else 
 	 {
-		say("The currentCall Object does not exist.");	 
+		say("Unfortunatly, your call could not be completed.");	 
 	 }
 ?>
